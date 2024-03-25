ab3eacf13efb33dde491c35c68587e8a365bbbc6
Hanseul Kim <scntkhs@sogang.ac.kr>
2024-03-26 00:56:24 +0900

    Hello World 2

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Hello World')
 
+    # adding new item in hotfix
+    print_hi('Hello World 2')
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
